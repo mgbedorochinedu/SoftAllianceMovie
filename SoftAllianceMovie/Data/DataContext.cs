08ce54bf44dbc72495569a3329f42a202ee7061e
Chinedu Mgbedoro <Chinedu.Mgbedoro@sterling.ng>
2023-11-06 22:26:28 +0100

    set up connection string and install nudget packages

---
+++ SoftAllianceMovie/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftAllianceMovie.Models;
+
+namespace SoftAllianceMovie.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+    }
+}
---
+++ SoftAllianceMovie/Models/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SoftAllianceMovie.Models
+{
+    public class Genre : BaseEntity
+    {
+        public int GenreId { get; set; }
+        public string GenreName { get; set; }
+
+        [ForeignKey(nameof(Movie))]
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+    }
+}
