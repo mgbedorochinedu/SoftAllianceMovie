4e7937aff734262ea362af7788c8cd4f02dff043
Chinedu Mgbedoro <Chinedu.Mgbedoro@sterling.ng>
2023-11-06 22:51:17 +0100

    Add base response

---
+++ SoftAllianceMovie/ServiceResponse/BaseResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace SoftAllianceMovie.ServiceResponse
+{
+    public class BaseResponse
+    {
+        public bool Success { get; set; }
+        public object Data { get; set; }
+        public string Message { get; set; }
+
+        public BaseResponse(bool success, object data, string message)
+        {
+            Success = success;
+            Data = data;
+            Message = message;
+        }
+    }
+}
